548a64cc5d3cad64b67c1ac7fa7229c0c4a6925e
Nicolas Cochard <ncochard@costar.co.uk>
2017-05-18 11:55:33 +0100

    Fixed issues

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-export function HelloWorld() {
-    return 'Hello world';
-}
+const str1 = 'Hello world';
+export const HelloWorld = () => str1;
 export function ShouldBeTreeShaken() {
     return 'This function is not used. It should be removed by the tree shaking process.';
 }
